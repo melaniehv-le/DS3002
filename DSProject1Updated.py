c216f1cb362fe9c2e886c1fac3aac89e70154d2e
melaniehv-le <91848778+melaniehv-le@users.noreply.github.com>
2021-11-07 21:46:19 -0500

    Project1

--- DSProject1Updated.py
+++ DSProject1Updated.py
@@ -13,7 +13,7 @@ df = pd.read_csv('https://query.data.world/s/tlvy3lwflzfeopkma3jpi5e5cubeb3')
 df.drop('Region', inplace=True, axis=1)
 #print(df)
 
-#Also imported the data files into a local file
+#Also imported the data files into a local file, this is the path at which the data set is located on my computer
 csvFilePath = r'/Users/melaniele/Documents/DSProject1/vital-statistics-suicide-deaths-by-age-group-race-ethnicity-resident-county-region-and-gender-beginn-1.csv'
 jsonFilePath = r'/Users/melaniele/Documents/DSProject1/vital-statistics-suicide-deaths-by-age-group-race-ethnicity-resident-county-region-and-gender-beginn-1.csv'
 
@@ -23,17 +23,19 @@ newjson = df.to_json("/Users/melaniele/Documents/DSProject1/vital-statistics-sui
 #print(newjson)
 
 #Defines a function that converts the CSV file to JSON
+#This starts with an empty array, then reads in the csv file, then appends it into a json array for each row
+#Then it converts the json array into a string, then writes it to a file
 def csv_to_json(csvFilePath, jsonFilePath):
     jsonArray = []
 
-    # read csv file
+    #First it reads in the csv file
     with open(csvFilePath, encoding='utf-8') as csvf:
         # load csv file data using csv library's dictionary reader
         csvReader = csv.DictReader(csvf)
 
-        # convert each csv row into python dict
+        #This converts each csv row into python dict
         for row in csvReader:
-            # add this python dict to json array
+            #then this is appended to a json array
             jsonArray.append(row)
 
     # convert python jsonArray to JSON String and write to file
@@ -41,10 +43,17 @@ def csv_to_json(csvFilePath, jsonFilePath):
         jsonString = json.dumps(jsonArray, indent=4)
         jsonf.write(jsonString)
 
+#This calls the function, based on the paths that I specified as to where the data file is located on my computer
 csv_to_json(csvFilePath, jsonFilePath)
+json.load(jsonString)
+
+
 
 #Operation Number 5: Generating summary of file including number of records and number of columns
 #Row index starts at 0, column index starts at 1
+#This counts the legnth of the columns and rows, and prints out the reuslt. I also wanted to see the actual column names so I printed that too
+#The result of this code shows that the data set used has 2076 rows and 7 columns.
+#The columns are ['Year', 'Race or Ethnicity', 'Sex', 'Age Group', 'Firearm Deaths','Alcohol-Related Deaths', 'Suicide Deaths']
 rowcount=len(df.axes[0])
 colcount=len(df.axes[1])
 print("Number of Rows: "+str(rowcount))
